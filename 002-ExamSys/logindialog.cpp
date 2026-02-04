fe0833307137e554f56a5b63b75fd339ec2813ed
saiumr <colaloveanan@gmail.com>
2026-02-04 17:55:59 +0800

    界面设计

--- 002-ExamSys/logindialog.cpp
+++ 002-ExamSys/logindialog.cpp
@@ -6,6 +6,15 @@ LoginDialog::LoginDialog(QWidget *parent)
     , ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
+    // label图片填充
+    ui->label_bg->setScaledContents(true);
+    // 窗口设置成背景图label大小
+    this->resize(ui->label_bg->width(), ui->label_bg->height());
+    // 设置窗体风格
+    this->setWindowTitle("驾校科目一考试登录");
+    // For Dialog windows, the Close button is enabled by default.
+    // this->setWindowFlags(Qt::Dialog | Qt::WindowCloseButtonHint);
+    this->setWindowFlags(Qt::Dialog);
 }
 
 LoginDialog::~LoginDialog()

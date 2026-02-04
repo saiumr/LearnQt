b92074ccdf46699574d7e8d8ab71c510e18c58ab
saiumr <colaloveanan@gmail.com>
2026-02-04 16:23:22 +0800

    驾校科目一考试系统：初始化

---
+++ 002-ExamSys/logindialog.h
@@ -0,0 +1,23 @@
+#ifndef LOGINDIALOG_H
+#define LOGINDIALOG_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class LoginDialog;  // Ui_LoginDialog的子类，用于描述登录窗口界面信息
+}
+QT_END_NAMESPACE
+
+class LoginDialog : public QDialog
+{
+    Q_OBJECT  // 继承自QObject类就有这个宏，支持信号槽机制
+
+public:
+    LoginDialog(QWidget *parent = nullptr);
+    ~LoginDialog();
+
+private:
+    Ui::LoginDialog *ui;
+};
+#endif // LOGINDIALOG_H

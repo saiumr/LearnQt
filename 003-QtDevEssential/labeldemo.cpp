46b5d1e28547c8ec96ec158371467e14fbf1d849
saiumr <colaloveanan@gmail.com>
2026-04-10 11:49:26 +0800

    qt: label自动换行，显示动图，label外框（话说使用ui设计比手写方便多了）

--- 003-QtDevEssential/labeldemo.cpp
+++ 003-QtDevEssential/labeldemo.cpp
@@ -1,6 +1,7 @@
 #include "labeldemo.h"
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QMovie>
 
 LabelDemo::LabelDemo(QWidget *parent) : QWidget(parent) {
     setWindowTitle("QLabel Demo");
@@ -10,6 +11,14 @@ LabelDemo::LabelDemo(QWidget *parent) : QWidget(parent) {
     // 普通文本
     QLabel *textLabel = new QLabel("普通文本标签", this);
     layout->addWidget(textLabel);
+
+    // 文本太长，自动换行
+    QLabel *longTextLabel = new QLabel("模拟超长文本，使用workWarp自动换行", this);
+    longTextLabel->setFixedWidth(128);
+    longTextLabel->setWordWrap(true);
+    longTextLabel->setFrameShape(QFrame::Box);
+    longTextLabel->setFrameShadow(QFrame::Plain);
+    layout->addWidget(longTextLabel);
 
     // 对齐方式
     QLabel *alignLabel = new QLabel("居中对齐", this);
@@ -31,6 +40,18 @@ LabelDemo::LabelDemo(QWidget *parent) : QWidget(parent) {
         imageLabel->setText("图片未找到");
     }
     layout->addWidget(imageLabel);
+
+    // 动态图片
+    QLabel *gifLabel = new QLabel(this);
+    QMovie *sang = new QMovie(":/sang.gif");
+    if (sang->isValid()) {
+        gifLabel->setMovie(sang);
+        gifLabel->setScaledContents(false);
+        sang->start();
+    } else {
+        qDebug() << "动图未找到";
+    }
+    layout->addWidget(gifLabel);
 
     // 富文本
     QLabel *htmlLabel = new QLabel("<b>粗体</b> 和 <i>斜体</i> 以及 <font color=red>红色</font>", this);

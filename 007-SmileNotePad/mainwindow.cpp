07d8abdba80b7f5d5bb94950d2469ba3ce6af840
saiumr <colaloveanan@gmail.com>
2026-08-07 11:12:03 +0800

    007 :D notepad ui

---
+++ 007-SmileNotePad/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle(":D Notpad");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
